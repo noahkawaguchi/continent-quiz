560a0597c343437b4b290c3d1cb29f2da3f13796
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2024-12-26 17:49:28 -0500

    Add question grading functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import './App.css'
+import { useState, useEffect } from 'react';
 import Header from './components/Header'
 import Question from './components/Question'
 import { useFetchAllCCA2 } from "./hooks/useFetchAllCCA2";
 import { Utils } from './utils/Utils';
 
 function App() {
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    console.log(score)
+  }, [score]);
+
+  const correctAnswer = (correct: boolean) => {
+    console.log(correct);
+    if (correct) {
+      setScore(prevScore => prevScore + 1);
+    }
+  }
+  
+  // Get all valid CCA2 codes only once
   const { cca2data, cca2loading, cca2error } = useFetchAllCCA2();
   if (cca2loading) return <p>Loading...</p>;
   if (cca2error) return <p>Error: {cca2error}</p>;
   const randomCCA2: string = Utils.randomCCA2(cca2data!);
 
-
   return (
     <>
       <header><Header /></header>
       <main>
-        <Question cca2={randomCCA2} />
+        <Question cca2={randomCCA2} correctAnswer={correctAnswer} />
       </main>
     </>
   )
--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -2,11 +2,30 @@ import React from "react";
 import { useProcessedData } from "../hooks/useProcessedData";
 
 interface QuestionProps {
-  cca2: string
+  cca2: string,
+  correctAnswer: Function
 }
 
-const Question: React.FC<QuestionProps> = ({ cca2 }) => {
+enum Continents {
+  Africa = 'Africa',
+  Antarctica = 'Antarctica',
+  Asia = 'Asia',
+  Europe = 'Europe',
+  North_America = 'North America',
+  Oceania = 'Oceania',
+  South_America = 'South America',
+}
+
+const Question: React.FC<QuestionProps> = ({ cca2, correctAnswer }) => {
   const { data, loading, error } = useProcessedData(cca2);
+
+  const gradeAnswer = (answer: Continents): void => {
+    if (data?.continents.includes(answer)) {
+      correctAnswer(true);
+      return;
+    }
+    correctAnswer(false);
+  }
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
@@ -16,13 +35,13 @@ const Question: React.FC<QuestionProps> = ({ cca2 }) => {
       <p>Where is...</p>
       <h3>{data?.commonName} {data?.flagEmoji}</h3>
       <div className="buttons">
-        <button>Africa</button>
-        <button>Antarctica</button>
-        <button>Asia</button>
-        <button>Europe</button>
-        <button>North America/Caribbean</button>
-        <button>Australia/Oceania</button>
-        <button>South America</button>
+        <button onClick={() => {gradeAnswer(Continents.Africa)}}>Africa</button>
+        <button onClick={() => {gradeAnswer(Continents.Antarctica)}}>Antarctica</button>
+        <button onClick={() => {gradeAnswer(Continents.Asia)}}>Asia</button>
+        <button onClick={() => {gradeAnswer(Continents.Europe)}}>Europe</button>
+        <button onClick={() => {gradeAnswer(Continents.North_America)}}>North America/Caribbean</button>
+        <button onClick={() => {gradeAnswer(Continents.Oceania)}}>Australia/Oceania</button>
+        <button onClick={() => {gradeAnswer(Continents.South_America)}}>South America</button>
       </div>
       <pre style={{textAlign: 'left'}}>{JSON.stringify(data, null, 2)}</pre>
     </div>
