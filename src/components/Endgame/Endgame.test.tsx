427f1a2fe50b6dba01b14f227314a14238aea776
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2024-12-27 16:23:45 -0500

    Write test for Endgame

---
+++ src/components/Endgame/Endgame.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import Endgame from "./Endgame";
+
+describe('Endgame', () => {
+  it('should call newGame when the button is clicked', async () => {
+    const user = userEvent.setup();
+    const newGame: Function = jest.fn();
+    render(<Endgame newGame={newGame} />);
+    const button: HTMLButtonElement = screen.getByText('Play Again?');
+    await user.click(button);
+    expect(newGame).toHaveBeenCalledTimes(1);
+  });
+});
