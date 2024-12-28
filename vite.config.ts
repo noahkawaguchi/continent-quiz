f28e4350632c91f1a513ff6ad2cc685cf544699d
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2024-12-28 17:28:07 -0500

    Deploy on GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/continent-quiz/',
+  build: {
+    outDir: 'dist', // Ensure output goes to the 'dist' directory
+    emptyOutDir: true, // Clean the output directory before building
+  },
+});
